c5ce9211baf5e44b788c240cebec6935080fef3c
opi919 <khandokermomen919@gmail.com>
2022-08-18 22:25:40 +0600

    first commit

---
+++ MyClient.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.net.*;
+
+public class MyClient{
+	public static void main(String[] args){
+		try{
+			Socket s = new Socket("10.110.51.94",6666);
+			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+			dout.writeUTF("Srity Hossain");
+			dout.flush();
+			dout.close();
+			s.close();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+}
